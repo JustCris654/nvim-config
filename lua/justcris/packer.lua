c129d6b74bf249fddd99a6bc16eb07bec6fa584f
JustCris <cristian.scapin654@icloud.com>
2023-08-15 21:22:52 +0200

    dired

--- lua/justcris/packer.lua
+++ lua/justcris/packer.lua
@@ -43,4 +43,18 @@ return require('packer').startup(function(use)
     }
 
     use('numToStr/Comment.nvim')
+
+    use {
+        "X3eRo0/dired.nvim",
+        requires = "MunifTanjim/nui.nvim",
+        config = function()
+            require("dired").setup {
+                path_separator = "/",
+                show_banner = false,
+                show_hidden = true,
+                show_dot_dirs = true,
+                show_colors = true,
+            }
+        end
+    }
 end)
--- lua/justcris/remap.lua
+++ lua/justcris/remap.lua
@@ -1,5 +1,9 @@
 vim.g.mapleader = " "
 
+-- save file
+vim.keymap.set("n", "<leader>fs", vim.cmd.update)
+vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format)
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -26,7 +30,6 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 -- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
@@ -39,4 +42,3 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
 -- vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
-
