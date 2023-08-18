d4cf0798cf8a702691e6d3e25d0804bf05397386
JustCris654 <cristian.scapin654@icloud.com>
2023-08-18 20:07:30 +0200

    fix remap

--- lua/justcris/remap.lua
+++ lua/justcris/remap.lua
@@ -1,63 +1,63 @@
-ciao.g.mapleader = " "
+vim.g.mapleader = " "
 
 -- save file
-ciao.keymap.set("n", "<leader>fs", ciao.cmd.update)
-ciao.keymap.set("n", "<leader>ff", function()
-	ciao.cmd.FormatWrite()
+vim.keymap.set("n", "<leader>fs", vim.cmd.update)
+vim.keymap.set("n", "<leader>ff", function()
+	vim.cmd.FormatWrite()
 end)
 
-ciao.keymap.set("n", "vig", "ggVG")
+vim.keymap.set("n", "vig", "ggVG")
 
 -- windows management
-ciao.keymap.set({ "n", "v" }, "<leader>wq", ciao.cmd.exit)
-ciao.keymap.set({ "n", "v" }, "<leader>wc", ciao.cmd.close)
+vim.keymap.set({ "n", "v" }, "<leader>wq", vim.cmd.exit)
+vim.keymap.set({ "n", "v" }, "<leader>wc", vim.cmd.close)
 -- and creation
-ciao.keymap.set({ "n", "v" }, "<leader>wv", ciao.cmd.vsplit)
-ciao.keymap.set({ "n", "v" }, "<leader>ws", ciao.cmd.split)
+vim.keymap.set({ "n", "v" }, "<leader>wv", vim.cmd.vsplit)
+vim.keymap.set({ "n", "v" }, "<leader>ws", vim.cmd.split)
 
 -- windows navigation
-ciao.keymap.set({ "n", "v" }, "<leader>ww", "<C-w>w")
-ciao.keymap.set({ "n", "v" }, "<leader>wh", "<C-w>h")
-ciao.keymap.set({ "n", "v" }, "<leader>wh", "<C-w>h")
-ciao.keymap.set({ "n", "v" }, "<leader>wl", "<C-w>l")
-ciao.keymap.set({ "n", "v" }, "<leader>wj", "<C-w>j")
-ciao.keymap.set({ "n", "v" }, "<leader>wk", "<C-w>k")
+vim.keymap.set({ "n", "v" }, "<leader>ww", "<C-w>w")
+vim.keymap.set({ "n", "v" }, "<leader>wh", "<C-w>h")
+vim.keymap.set({ "n", "v" }, "<leader>wh", "<C-w>h")
+vim.keymap.set({ "n", "v" }, "<leader>wl", "<C-w>l")
+vim.keymap.set({ "n", "v" }, "<leader>wj", "<C-w>j")
+vim.keymap.set({ "n", "v" }, "<leader>wk", "<C-w>k")
 
-ciao.keymap.set("n", "<leader>pv", ciao.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-ciao.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-ciao.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-ciao.keymap.set("n", "J", "mzJ`z")
-ciao.keymap.set("n", "<C-d>", "<C-d>zz")
-ciao.keymap.set("n", "<C-u>", "<C-u>zz")
-ciao.keymap.set("n", "n", "nzzzv")
-ciao.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 
 -- greatest remap ever
-ciao.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
 -- yank selection to system clipboard
-ciao.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-ciao.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-ciao.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- This is going to get me cancelled
-ciao.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("i", "<C-c>", "<Esc>")
 
-ciao.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "Q", "<nop>")
 -- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
-ciao.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-ciao.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-ciao.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-ciao.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-ciao.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 -- leader x makes the current file executable
-ciao.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
 -- vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
