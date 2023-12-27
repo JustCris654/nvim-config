b4919292bf69c939cbea9d08e7678d15cbaef13d
JustCris <cristian.scapin654@icloud.com>
2023-12-27 17:09:01 +0100

    formatter and icons

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -1,76 +1,76 @@
 -- Utilities for creating configurations
-local util = require "formatter.util"
+local util = require("formatter.util")
 
 -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-require("formatter").setup {
-    -- Enable or disable logging
-    logging = true,
-    -- Set the log level
-    log_level = vim.log.levels.WARN,
-    -- All formatter configurations are opt-in
-    filetype = {
-        -- Formatter configurations for filetype "lua" go here
-        -- and will be executed in order
-        lua = {
-            -- "formatter.filetypes.lua" defines default configurations for the
-            -- "lua" filetype
-            require("formatter.filetypes.lua").stylua,
+require("formatter").setup({
+	-- Enable or disable logging
+	logging = true,
+	-- Set the log level
+	log_level = vim.log.levels.WARN,
+	-- All formatter configurations are opt-in
+	filetype = {
+		-- Formatter configurations for filetype "lua" go here
+		-- and will be executed in order
+		lua = {
+			-- "formatter.filetypes.lua" defines default configurations for the
+			-- "lua" filetype
+			require("formatter.filetypes.lua").stylua,
 
-            -- You can also define your own configuration
-            function()
-                -- Supports conditional formatting
-                if util.get_current_buffer_file_name() == "special.lua" then
-                    return nil
-                end
+			-- You can also define your own configuration
+			function()
+				-- Supports conditional formatting
+				if util.get_current_buffer_file_name() == "special.lua" then
+					return nil
+				end
 
-                -- Full specification of configurations is down below and in Vim help
-                -- files
-                return {
-                    exe = "stylua",
-                    args = {
-                        "--search-parent-directories",
-                        "--stdin-filepath",
-                        util.escape_path(util.get_current_buffer_file_path()),
-                        "--",
-                        "-",
-                    },
-                    stdin = true,
-                }
-            end
-        },
+				-- Full specification of configurations is down below and in Vim help
+				-- files
+				return {
+					exe = "stylua",
+					args = {
+						"--search-parent-directories",
+						"--stdin-filepath",
+						util.escape_path(util.get_current_buffer_file_path()),
+						"--",
+						"-",
+					},
+					stdin = true,
+				}
+			end,
+		},
 
-        typescript = {
-            require("formatter.filetypes.typescript").prettier,
-        },
+		typescript = {
+			require("formatter.filetypes.typescript").prettier,
+		},
 
-        rust = {
-            function()
-                return {
-                    exe = "rustfmt",
-                    args = {
-                        "--edition 2021"
-                    },
-                    stdin = true,
-                }
-            end
-        },
+		rust = {
+			function()
+				return {
+					exe = "rustfmt",
+					args = {
+						"--edition 2021",
+					},
+					stdin = true,
+				}
+			end,
+		},
 
-        c = {
-            require("formatter.filetypes.c").clangformat,
-        },
+		c = {
+			require("formatter.filetypes.c").clangformat,
+		},
 
-        cpp = {
-            require("formatter.filetypes.c").clangformat,
-        },
+		cpp = {
+			require("formatter.filetypes.c").clangformat,
+		},
 
-        zig = {
-            require("formatter.filetypes.zig").zigfmt,
-        },
+		zig = {
+			require("formatter.filetypes.zig").zigfmt,
+		},
 
-        ["*"] = {
-            function()
-                vim.lsp.buf.format();
-            end
-        }
-    }
-}
+		["*"] = {
+			function()
+				vim.lsp.buf.format()
+			end,
+		},
+	},
+})
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,15 +1,15 @@
-local builtin = require('telescope.builtin')
+local telescope = require("telescope.builtin")
 
-vim.keymap.set('n', '<leader><leader>', builtin.git_files, {})
+vim.keymap.set("n", "<leader><leader>", telescope.git_files, {})
 
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>sp', function()
-    builtin.grep_string({
-        search = vim.fn.input("Grep > ")
-    })
+vim.keymap.set("n", "<leader>pf", telescope.find_files, {})
+vim.keymap.set("n", "<leader>sp", function()
+	telescope.grep_string({
+		search = vim.fn.input("Grep > "),
+	})
 end)
-vim.keymap.set('n', '<leader>sf', builtin.live_grep, {})
+vim.keymap.set("n", "<leader>sf", telescope.live_grep, {})
 
-vim.keymap.set('n', '<leader>bi', builtin.buffers, {})
+vim.keymap.set("n", "<leader>bi", telescope.buffers, {})
 
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set("n", "<leader>fh", telescope.help_tags, {})
