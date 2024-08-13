b978215f652cc89ea06f858a4eab93ed177102a7
JustCris <cristian.scapin654@icloud.com>
2024-08-13 11:42:13 +0200

    treesitter dont update on start

--- lua/justcris/lazy/treesitter.lua
+++ lua/justcris/lazy/treesitter.lua
@@ -14,7 +14,7 @@ return {
 
             -- Automatically install missing parsers when entering buffer
             -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-            auto_install = true,
+            auto_install = false,
 
             indent = {
                 enable = true
@@ -32,15 +32,5 @@ return {
             },
         })
 
-        local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-        treesitter_parser_config.templ = {
-            install_info = {
-                url = "https://github.com/vrischmann/tree-sitter-templ.git",
-                files = {"src/parser.c", "src/scanner.c"},
-                branch = "master",
-            },
-        }
-
-        vim.treesitter.language.register("templ", "templ")
     end
 }
