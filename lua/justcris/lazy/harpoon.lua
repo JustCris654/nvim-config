d474c952fd0129bdd7ffc917efcdc2e269970571
JustCris <cristian.scapin654@icloud.com>
2024-10-24 11:36:11 +0200

    todo-comments

--- lua/justcris/lazy/harpoon.lua
+++ lua/justcris/lazy/harpoon.lua
@@ -10,14 +10,14 @@ return {
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
-        vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
-        vim.keymap.set("n", "<leader><C-t>", function() harpoon:list():replace_at(2) end)
-        vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(3) end)
-        vim.keymap.set("n", "<leader><C-s>", function() harpoon:list():replace_at(4) end)
+        vim.keymap.set("n", "<A-h>", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<A-j>", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<A-k>", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<A-l>", function() harpoon:list():select(4) end)
+        -- vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
+        -- vim.keymap.set("n", "<leader><C-t>", function() harpoon:list():replace_at(2) end)
+        -- vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(3) end)
+        -- vim.keymap.set("n", "<leader><C-s>", function() harpoon:list():replace_at(4) end)
     end
 }
 
--- lua/justcris/remap.lua
+++ lua/justcris/remap.lua
@@ -39,8 +39,8 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set("n", "Q", "<nop>")
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<C-K>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-J>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
